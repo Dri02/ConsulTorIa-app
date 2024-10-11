fb294c3005464e3bce84cacec4ebb34d27f94bbc
Dalia <daliarisla2002@gmail.com>
2024-10-11 11:04:46 -0400

    Update

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      "@babel/plugin-transform-runtime",
+      "@babel/plugin-proposal-class-properties",
       "react-native-reanimated/plugin",
     ],
   };
